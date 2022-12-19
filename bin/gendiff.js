23c3e81d6b34243d53902cae0a0ad822844ec4fa
feot <imboutodis@gmail.com>
2022-12-19 23:00:50 +0300

    add files arguments and output format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,9 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number');
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
