bb62fe85b66b734cea12b9690517a9ba2875aacb
feot <imboutodis@gmail.com>
2023-01-03 23:02:57 +0300

    add yaml support

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
-import { existsSync, readFileSync } from 'fs';
-import { resolve as pathResolve, extname } from 'path';
 import _ from 'lodash';
-
-const getDataParse = (filepath) => {
-  const workingDir = process.cwd();
-  const absFilepath = (existsSync(filepath)) ? filepath : pathResolve(workingDir, filepath);
-  const fileExtension = extname(filepath);
-
-  switch (fileExtension) {
-    case '.json':
-      return JSON.parse(readFileSync(absFilepath), 'utf8');
-    default:
-      throw new Error(`Wrong file extension: ${fileExtension}`);
-  }
-};
+import parsers from './parsers.js';
 
 const genDiff = (filepath1, filepath2) => {
-  const dataParse1 = getDataParse(filepath1);
-  const dataParse2 = getDataParse(filepath2);
+  const dataParse1 = parsers(filepath1);
+  const dataParse2 = parsers(filepath2);
   const keys = _.union(Object.keys(dataParse1), Object.keys(dataParse2)).sort();
 
   const diff = keys.map((key) => {
---
+++ src/parsers.js
@@ -0,0 +1,18 @@
+import yaml from 'js-yaml';
+import { existsSync, readFileSync } from 'fs';
+import { resolve as pathResolve, extname } from 'path';
+
+export default (filepath) => {
+  const workingDir = process.cwd();
+  const absFilepath = (existsSync(filepath)) ? filepath : pathResolve(workingDir, filepath);
+  const fileExtension = extname(filepath);
+
+  switch (fileExtension) {
+    case '.json':
+      return JSON.parse(readFileSync(absFilepath), 'utf8');
+    case '.yaml':
+      return yaml.load(readFileSync(absFilepath), 'utf8');
+    default:
+      throw new Error(`Wrong file extension: ${fileExtension}`);
+  }
+};
