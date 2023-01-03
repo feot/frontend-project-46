dea4fb261de6edcc16f02cb6916694ba37d00651
feot <imboutodis@gmail.com>
2023-01-03 22:02:01 +0300

    add yaml diff test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,12 +1,19 @@
 import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
-import jsonDiffResult from '../__fixtures__/test_results.js';
+import diffResult from '../__fixtures__/test_results.js';
 
 test('json', () => {
   const file1Path = '__fixtures__/file1.json';
   const file2Path = '__fixtures__/file2.json';
 
-  expect(genDiff(file1Path, file2Path)).toEqual(jsonDiffResult);
+  expect(genDiff(file1Path, file2Path)).toEqual(diffResult);
+});
+
+test('yaml', () => {
+  const file1Path = '__fixtures__/file1.yaml';
+  const file2Path = '__fixtures__/file2.yaml';
+
+  expect(genDiff(file1Path, file2Path)).toEqual(diffResult);
 });
 
 test('unsupported file extension', () => {
