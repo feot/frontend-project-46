e9023356521b0e37c94c866f723f3420f81369b1
feot <imboutodis@gmail.com>
2023-01-12 00:41:13 +0300

    update tests to match nested files

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -2,25 +2,27 @@ import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
 import diffResult from '../__fixtures__/test_results.js';
 
-test('json', () => {
+test('json', async () => {
   const file1Path = '__fixtures__/file1.json';
   const file2Path = '__fixtures__/file2.json';
+  const receivedResult = await genDiff(file1Path, file2Path);
 
-  expect(genDiff(file1Path, file2Path)).toEqual(diffResult);
+  expect(receivedResult).toEqual(diffResult);
 });
 
-test('yaml', () => {
+test('yaml', async () => {
   const file1Path = '__fixtures__/file1.yaml';
   const file2Path = '__fixtures__/file2.yaml';
+  const receivedResult = await genDiff(file1Path, file2Path);
 
-  expect(genDiff(file1Path, file2Path)).toEqual(diffResult);
+  expect(receivedResult).toEqual(diffResult);
 });
 
-test('unsupported file extension', () => {
+test('unsupported file extension', async () => {
   const file1Path = '__fixtures__/file1.pdf';
   const file2Path = '__fixtures__/file2.pdf';
 
-  expect(() => {
-    genDiff(file1Path, file2Path);
-  }).toThrow();
+  await expect(genDiff(file1Path, file2Path))
+    .rejects
+    .toThrow();
 });
