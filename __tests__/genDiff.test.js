8575c0c2370489a665677fc74cd45940eb4ee01e
feot <imboutodis@gmail.com>
2023-03-16 00:03:14 +0300

    add plain-style diff test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,13 +1,13 @@
 import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
-import diffResult from '../__fixtures__/test_results.js';
+import { resultPlain, resultStylish } from '../__fixtures__/test_results.js';
 
 test('json', async () => {
   const file1Path = '__fixtures__/file1.json';
   const file2Path = '__fixtures__/file2.json';
   const receivedResult = await genDiff(file1Path, file2Path);
 
-  expect(receivedResult).toEqual(diffResult);
+  expect(receivedResult).toEqual(resultStylish);
 });
 
 test('yaml', async () => {
@@ -15,7 +15,15 @@ test('yaml', async () => {
   const file2Path = '__fixtures__/file2.yaml';
   const receivedResult = await genDiff(file1Path, file2Path);
 
-  expect(receivedResult).toEqual(diffResult);
+  expect(receivedResult).toEqual(resultStylish);
+});
+
+test('plain style', async () => {
+  const file1Path = '__fixtures__/file1.yaml';
+  const file2Path = '__fixtures__/file2.yaml';
+  const receivedResult = await genDiff(file1Path, file2Path, 'plain');
+
+  expect(receivedResult).toEqual(resultPlain);
 });
 
 test('unsupported file extension', async () => {
