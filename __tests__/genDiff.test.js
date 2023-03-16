7280b615059b8eb6a156c4216f6ef50ceecfc3c9
feot <imboutodis@gmail.com>
2023-03-16 23:35:57 +0300

    switch yaml test to yml

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -11,16 +11,16 @@ test('json', async () => {
 });
 
 test('yaml', async () => {
-  const file1Path = '__fixtures__/file1.yaml';
-  const file2Path = '__fixtures__/file2.yaml';
+  const file1Path = '__fixtures__/file1.yml';
+  const file2Path = '__fixtures__/file2.yml';
   const receivedResult = await genDiff(file1Path, file2Path);
 
   expect(receivedResult).toEqual(resultStylish);
 });
 
 test('plain style', async () => {
-  const file1Path = '__fixtures__/file1.yaml';
-  const file2Path = '__fixtures__/file2.yaml';
+  const file1Path = '__fixtures__/file1.yml';
+  const file2Path = '__fixtures__/file2.yml';
   const receivedResult = await genDiff(file1Path, file2Path, 'plain');
 
   expect(receivedResult).toEqual(resultPlain);
