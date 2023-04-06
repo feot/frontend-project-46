d2e867d4090f259d813fca3769b15b5f67e76605
feot <imboutodis@gmail.com>
2023-04-07 02:30:10 +0300

    refactor genDiff with functional approach

--- src/index.js
+++ src/index.js
@@ -1,52 +1,62 @@
 import _ from 'lodash';
-import parsers from './parsers.js';
+import parser from './parsers.js';
 import formatterRouter from './formatters/formatterRouter.js';
 
 const genDiff = (filepath1, filepath2, format) => {
-  const dataParse1 = parsers(filepath1);
-  const dataParse2 = parsers(filepath2);
+  const dataParse1 = parser(filepath1);
+  const dataParse2 = parser(filepath2);
 
   const iter = (data1, data2) => {
     const keys = _.union(Object.keys(data1), Object.keys(data2)).sort();
-    const values = [];
 
-    keys.forEach((key) => {
-      let file1Value = data1[key];
-      let file2Value = data2[key];
-      const isFile1KeyExist = !(file1Value === undefined);
-      const isFile2KeyExist = !(file2Value === undefined);
-      let children = null;
+    return keys
+      .map((key) => {
+        const file1Value = data1[key];
+        const file2Value = data2[key];
+        const isFile1KeyExist = !(file1Value === undefined);
+        const isFile2KeyExist = !(file2Value === undefined);
 
-      if (_.isObject(file1Value) && _.isObject(file2Value)) {
-        children = iter(file1Value, file2Value);
-        file1Value = null;
-        file2Value = null;
-      }
-
-      if (isFile1KeyExist) {
-        const state = (file1Value === file2Value) ? 'stayed' : 'removed';
-        values.push({
-          key,
-          value: file1Value,
-          state,
-          children,
-        });
-        if (state === 'stayed') {
-          return;
+        if (_.isObject(file1Value) && _.isObject(file2Value)) {
+          return [{
+            key,
+            value: null,
+            state: 'stayed',
+            children: iter(file1Value, file2Value),
+          }];
         }
-      }
-      if (isFile2KeyExist) {
-        const state = (file1Value === file2Value) ? 'stayed' : 'added';
-        values.push({
-          key,
-          value: file2Value,
-          state,
-          children,
-        });
-      }
-    });
 
-    return values;
+        if (isFile1KeyExist && isFile2KeyExist && file1Value !== file2Value) {
+          return [
+            {
+              key,
+              value: file1Value,
+              state: 'removed',
+              children: null,
+            },
+            {
+              key,
+              value: file2Value,
+              state: 'added',
+              children: null,
+            },
+          ];
+        }
+
+        const state = (() => {
+          if (isFile1KeyExist) {
+            return (file1Value === file2Value) ? 'stayed' : 'removed';
+          }
+          return (file1Value === file2Value) ? 'stayed' : 'added';
+        })();
+
+        return [{
+          key,
+          value: (isFile1KeyExist) ? file1Value : file2Value,
+          state,
+          children: null,
+        }];
+      })
+      .flat();
   };
 
   const differenceTree = {
